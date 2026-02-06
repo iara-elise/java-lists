afe79fe3a8e10825d12ccec6125b8cf2f2021a47
iara-elise <iara.elise.0006@gmail.com>
2026-02-05 23:43:34 -0300

    Close input.

--- L3-Loop/Q13.java
+++ L3-Loop/Q13.java
@@ -18,6 +18,8 @@ public class Q13 {
         System.out.print("Enter a number between 1 and 10: ");
         int num = input.nextInt();
 
+        input.close();
+
         for(int i = 1; i <= 10; i++) {
             System.out.println(i + " x " + num + " = " + num * i);
 
