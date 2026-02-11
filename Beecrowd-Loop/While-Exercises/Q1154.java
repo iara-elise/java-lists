bb4bdcf2b7fced2138b801323e17d985f9059804
iara-elise <iara.elise.0006@gmail.com>
2026-02-11 19:54:48 -0300

    Update solution for exercise 1154.

--- Beecrowd-Loop/While-Exercises/Q1154.java
+++ Beecrowd-Loop/While-Exercises/Q1154.java
@@ -1,7 +1,7 @@
 /* Beecrowd - Questão 1154 */
 
 /* Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo.
-O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos. 
+O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcule e imprima a idade média deste grupo de indivíduos. 
 
 ENTRADA: A entrada contém um número indeterminado de inteiros. A entrada será encerrada quando um valor negativo for lido.
 
@@ -14,20 +14,21 @@ public class Q1154 {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter some ages:");
-
         int ages = input.nextInt();
-        
-        float agesSum = ages;
-        int count = 0;
 
-        while (ages > 0) {
+        int countPeoples = 0;
+        double sum = 0;
+
+        while (ages >= 0) {
+
+            countPeoples++;
+            sum += ages;
+
             ages = input.nextInt();
-            agesSum += ages;
-            count++;
         }
 
         input.close();
-        System.out.printf("Average age: %.2f%n", agesSum / count);
+
+        System.out.printf("Average age: %.2f%n", sum / countPeoples);
     }
 }
