b80cd86e9a69f7017ebe714dcd0888db96e34acb
iara-elise <iara.elise.0006@gmail.com>
2026-02-17 18:03:38 -0300

    Add .gitignore and question 1067 from beecrowd exercises.

---
+++ Beecrowd-Loop/For-Exercises/Q1067.java
@@ -0,0 +1,25 @@
+/* Beecrowd - Questão 1067 */
+
+/* Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha,
+inclusive o X, se for o caso. */
+
+import java.util.Scanner;
+
+public class Q1067 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        input.close();
+    }
+}
